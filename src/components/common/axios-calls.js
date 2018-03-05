9b028ec7df4297f6f14f5e66a392999eae701500
mwaz <amby.mwaz@gmail.com>
2018-03-05 20:07:49 +0300

    [bg #155710913] create component for globally declaring axios calls

---
+++ src/components/common/axios-calls.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+/**
+ * Component for handling Api calls without duplicating in each file.
+ */
+const axiosInstance = axios.create({
+    baseURL: 'http://127.0.0.1:5000/yummy_api/v1',
+    headers: {
+        Authorization: `${localStorage.getItem('token')}`,
+       
+}});
+
+axiosInstance.interceptors.request.use((config) => {
+    if (localStorage.getItem('token') && config.headers.Authorization === 'null') {
+        config.headers.Authorization = `${localStorage.getItem('token')}`;
+    }
+    return config;
+});
+
+export default axiosInstance;
