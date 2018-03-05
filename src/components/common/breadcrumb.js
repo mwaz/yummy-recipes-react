95ba711b791848bef36891988c7e535a8c8cbdda
mwaz <amby.mwaz@gmail.com>
2018-03-05 20:03:56 +0300

    [bg #155710913] crete reusable breadcrumb component

---
+++ src/components/common/breadcrumb.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Row, Col, Breadcrumb} from 'react-bootstrap';
+
+const BreadCrumbComponent = (props) => {
+    return (
+        <Row >
+        <Col sm={12}>
+            <Breadcrumb>
+                <Breadcrumb.Item href={props.href} >{props.item_one}</Breadcrumb.Item>
+                <Breadcrumb.Item active href="#" >{props.active_item}</Breadcrumb.Item>
+            </Breadcrumb>
+        </Col>
+        </Row >
+    );
+}
+
+export default BreadCrumbComponent
