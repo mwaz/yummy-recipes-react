8acfe7a85ab4915378921cc8ccc3c38637e94fd3
mwaz <amby.mwaz@gmail.com>
2018-03-07 10:38:53 +0300

    [ch #155710913] add documentation for index.js file

--- src/index.js
+++ src/index.js
@@ -7,11 +7,15 @@ import Login from "./components/auth-front/login.js";
 import Categories from "./components/categories-front/categories.js";
 import Recipes from "./components/recipes-front/recipes.js";
 import Private from "./containers/private.js";
-// import Public from "./containers/public.js";
+import NotFound from './components/common/not-found';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 
+/**
+ * Component to handle all the routes and display them to the user according
+ * to the URL's
+ */
 
 ReactDOM.render(
         <Router>
@@ -20,8 +24,7 @@ ReactDOM.render(
             <Route exact path="/login" component={Login} />
             <Private exact path="/categories/" component={Categories} />
             <Private exact path="/categories/:category_id/recipes/" component={Recipes} />
-
-            
+            <Route component={NotFound} />
         </Switch>
    </Router>,
     document.getElementById("container")
