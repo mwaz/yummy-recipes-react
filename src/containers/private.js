158a639cc2525e29b057251757909bb659b25027
mwaz <amby.mwaz@gmail.com>
2018-03-07 10:34:43 +0300

    [ch #155710913] fix documnetation on private route

--- src/containers/private.js
+++ src/containers/private.js
@@ -1,14 +1,17 @@
 import { Redirect, Route } from 'react-router';
 import React from 'react';
+
+/**
+ * Component responsible for protecting routes
+ */
 const Private = ({ component: Values, ...rest }) => (
-    <Route {...rest} render={props => (
-        window.localStorage.getItem("token") ? (<Values {...props} />
-        ) : (
-            
-                <Redirect to={{
-                    pathname: '/login',
-                }} />
-            )
-    )} />
-)
+  <Route
+    {...rest}
+    render={props => (
+          window.localStorage.getItem('token') ? 
+          (<Values {...props} />) :
+           (<Redirect to={{ pathname: '/login' }} />
+        ))}
+  />
+);
 export default Private
